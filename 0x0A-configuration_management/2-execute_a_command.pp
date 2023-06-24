5322cd3060884e152cb1e05687ab4c9fd4bd8d64
keme-ebi <kelvin6bolou@gmail.com>
2023-06-24 04:25:20 +0000

    a script that creates a manifest that kills a process called killmenowls

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,7 @@
 # Creates a manifest that kills a process named killmenow
 
 exec { 'killmenow_process':
-  command     => 'pkill killmenow',
+  command     => 'pkill -f killmenow',
   path        => 'usr/bin:/usr/sbin:/bin:/sbin',
   refreshonly => true,
 }

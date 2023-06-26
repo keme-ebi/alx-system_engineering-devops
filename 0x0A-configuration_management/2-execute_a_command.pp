0935284a8018b914e1bb988b852428f85f3ce080
keme-ebi <kelvin6bolou@gmail.com>
2023-06-26 03:09:54 +0000

    adjusted according to puppet-lint

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # Creates a manifest that kills a process named killmenow
 
 exec { 'pkill killmenow':
-  command     => 'pkill -f killmenow',
-  path        => '/usr/bin',
+  command => 'pkill -f killmenow',
+  path    => '/usr/bin',
 }

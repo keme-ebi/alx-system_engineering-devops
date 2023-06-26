3cc0650024bec98246aeba90d8dbac4f0806a28a
keme-ebi <kelvin6bolou@gmail.com>
2023-06-26 03:04:43 +0000

    removed -f

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,7 @@
 # Creates a manifest that kills a process named killmenow
 
 exec { 'pkill killmenow':
-  command     => 'pkill killmenow',
+  command     => 'pkill -f killmenow',
   path        => '/usr/bin',
   refreshonly => true,
 }

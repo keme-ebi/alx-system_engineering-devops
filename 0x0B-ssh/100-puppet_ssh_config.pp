b33ca7a2b2f074823d1dd819a06f9945de073f23
keme-ebi <kelvin6bolou@gmail.com>
2023-06-26 02:05:25 +0000

    setup client ssh configuration file using puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,6 @@
 # Set up client SSH configuration in order to connect without password
 
-class { 'sshkeys::client'@
+class { 'sshkeys::client':
     ensure                  => 'present'
     identity_file           => '~/.ssh/school',
     password_authentication => 'no',

d34ddeb1fce0d426e5e9cc1b61c7484a819d65ca
keme-ebi <kelvin6bolou@gmail.com>
2023-06-26 02:56:58 +0000

    added pkill to exec

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # Creates a manifest that kills a process named killmenow
 
-exec { 'killmenow':
+exec { 'pkill killmenow':
   command     => 'pkill -f killmenow',
   path        => '/usr/bin',
   refreshonly => true,

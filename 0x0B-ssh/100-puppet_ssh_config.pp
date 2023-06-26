15f6b2fbf64af7ce28fd582b9e588f9c12323986
keme-ebi <kelvin6bolou@gmail.com>
2023-06-26 02:43:54 +0000

    setup client ssh configuration file using puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,9 @@
 # Set up client SSH configuration in order to connect without password
 
-file { '~/.ssh/config':
-    ensure  => 'present'
-    content => '
-      # SSH client configuration
+file { "/usr/bin":
+    ensure  => present,
+    content => "
       Host 34.207.189.167
         IdentityFile ~/.ssh/school
-        PasswordAuthentication no
-    ',
+        PasswordAuthentication no",
 }

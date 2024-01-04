7abfc3ad04ec1e4173ded8f55458f678342cde11
keme-ebi <kelvin6bolou@gmail.com>
2024-01-04 03:10:47 +0000

    a script that for a given employee id, returns information about his/her todo list progress

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""returns information about an employees todo list progress"""
+
+import requests
+import json
+import sys
+
+
+if __name__ == "__main__":
+    e_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com"
+    # get the employees name
+    req = requests.get("{}/users/{}".format(url, e_id))
+    data = req.json()
+    name = data.get('name')
+
+    # get the number of tasks done and total number of tasks
+    req = requests.get("{}/todos?userId={}".format(url, e_id))
+    data = req.json()
+    total = len(data)
+    t_done = sum(tasks['completed'] for tasks in data)
+
+    print("Employee {} is done with tasks({}/{}):".format(name, t_done, total))
+    for tasks in data:
+        if tasks['completed']:
+            print("\t {}".format(tasks.get('title')))
